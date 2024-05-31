5a1f3df09034f084c6d8590e785fc7a86c269c62
cyb3rgh05t <flammang.yves@gmail.com>
2024-05-31 16:26:31 +0200

    Update message.py

--- app/bot/helper/message.py
+++ app/bot/helper/message.py
@@ -1,5 +1,47 @@
+from pickle import FALSE
+import app.bot.helper.jellyfinhelper as jelly
+from app.bot.helper.textformat import bcolors
 import discord
+from discord.ext import commands
+from discord import app_commands
 from datetime import timedelta, datetime
+import asyncio
+from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
+import app.bot.helper.db as db
+import app.bot.helper.plexhelper as plexhelper
+import app.bot.helper.jellyfinhelper as jelly
+import texttable
+from app.bot.helper.message import *
+from app.bot.helper.confighelper import *
+
+CONFIG_PATH = 'app/config/config.ini'
+BOT_SECTION = 'bot_envs'
+
+plex_configured = True
+jellyfin_configured = True
+
+config = configparser.ConfigParser()
+config.read(CONFIG_PATH)
+
+plex_token_configured = True
+try:
+    PLEX_TOKEN = config.get(BOT_SECTION, 'plex_token')
+    PLEX_BASE_URL = config.get(BOT_SECTION, 'plex_base_url')
+except:
+    print("No Plex auth token details found")
+    plex_token_configured = False
+
+# Get Plex config
+try:
+    PLEXUSER = config.get(BOT_SECTION, 'plex_user')
+    PLEXPASS = config.get(BOT_SECTION, 'plex_pass')
+    PLEX_SERVER_NAME = config.get(BOT_SECTION, 'plex_server_name')
+except:
+    print("No Plex login info found")
+    if not plex_token_configured:
+        print("Could not load plex config")
+        plex_configured = False
 
 # these were copied from the app object. They could be made static instead but I'm lazy.
 async def embederror(recipient, message, ephemeral=True):
